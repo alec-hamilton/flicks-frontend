075245f3cdb8ebe10b017a41e14cf3fa05a7f898
alech <hamiltonalec@gmail.com>
2023-03-16 11:32:37 +0000

    feat: links to pages added (minus about dropdown)

--- pages/components/Layout/Navbar/DesktopMenu.tsx
+++ pages/components/Layout/Navbar/DesktopMenu.tsx
@@ -1,28 +1,31 @@
+import Link from "next/link";
 import { BiChevronDown, BiSearch } from "react-icons/bi";
 
 const DesktopMenu = () => {
   return (
-    <div className="hidden mdx:flex ml-4 gap-2 justify-between w-full">
-      <div className="flex gap-2">
-        <button className="border border-primary text-primary px-4">
-          Browse
-        </button>
-        <button className="border border-primary text-primary px-4">
-          Cinemas
-        </button>
-        <button className="border border-primary text-primary px-4">
-          Contact
-        </button>
-        <button className="border border-primary text-primary">
-          <span className="flex items-center h-full">
-            <span className="px-4">About</span>
-            <span className="flex items-center border-l h-full px-1">
-              <BiChevronDown size="1.5rem" />
+    <nav className="hidden mdx:flex ml-4 gap-2 justify-between w-full">
+      <ul className="flex gap-2">
+        <li className="flex items-center border border-primary text-primary px-4">
+          <Link href="/browse">Browse</Link>
+        </li>
+        <li className="flex items-center border border-primary text-primary px-4">
+          <Link href="/cinemas">Cinemas</Link>
+        </li>
+        <li className="flex items-center border border-primary text-primary px-4">
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li className="flex">
+          <button type="button" className="border border-primary text-primary">
+            <span className="flex items-center h-full">
+              <span className="px-4">About</span>
+              <span className="flex items-center border-l h-full px-1">
+                <BiChevronDown size="1.5rem" />
+              </span>
             </span>
-          </span>
-        </button>
-      </div>
-      <button className="border border-primary text-primary px-4">
+          </button>
+        </li>
+      </ul>
+      <button type="button" className="border border-primary text-primary px-4">
         <span className="flex gap-4">
           <span>
             <BiSearch size="1.5rem" />
@@ -30,7 +33,7 @@ const DesktopMenu = () => {
           <span>Search</span>
         </span>
       </button>
-    </div>
+    </nav>
   );
 };
 
--- pages/components/Layout/Navbar/NavLogo.tsx
+++ pages/components/Layout/Navbar/NavLogo.tsx
@@ -1,15 +1,19 @@
+import Link from "next/link";
+
 const NavLogo = () => {
   return (
-    <div className="flex flex-col border border-primary">
-      <div className="border-b border-primary">
-        <p className="text-primary px-1 text-xs">Welcome to...</p>
+    <Link href="/">
+      <div className="flex flex-col border border-primary">
+        <div className="border-b border-primary">
+          <p className="text-primary px-1 text-xs">Welcome to...</p>
+        </div>
+        <div className="flex flex-col items-center justify-center h-full">
+          <p className="text-primary py-2 px-4 font-logo text-l whitespace-nowrap">
+            20th Century Flicks
+          </p>
+        </div>
       </div>
-      <div className="flex flex-col items-center justify-center h-full">
-        <p className="text-primary py-2 px-4 font-logo text-l whitespace-nowrap">
-          20th Century Flicks
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
