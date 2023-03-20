5e75130854c10dc4c4dc1fc57462b77ac8c9c588
alech <hamiltonalec@gmail.com>
2023-03-20 12:15:31 +0000

    feat: images added

--- pages/cinemas.tsx
+++ pages/cinemas.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import StandardButton from "./components/buttons/StandardButton";
+import twinPeaksCinema from "../assets/images/twin-peaks-style-cinema.png";
+import videodromeCinema from "../assets/images/bladerunner-style-cinema.png";
 
 const Cinemas = () => {
   return (
@@ -10,6 +13,7 @@ const Cinemas = () => {
           18. For more details or to check availability and book a screening..
           COPY CHANGE NEEDED?
         </p>
+
         <div className="flex flex-col gap-3">
           <StandardButton>Book a cinema</StandardButton>
           <StandardButton>Take virtual tour</StandardButton>
@@ -22,6 +26,11 @@ const Cinemas = () => {
             Our Twin Peaks-y Kino seats 8 people and The Videodrome seats up to
             18. For more details or to check availability and book a screening
           </p>
+          <Image
+            src={twinPeaksCinema}
+            alt="Cinema auditorium in the style of twin peaks"
+            className="border border-primary mb-4"
+          />
           <StandardButton>Book the Kino</StandardButton>
         </div>
       </div>
@@ -32,6 +41,11 @@ const Cinemas = () => {
             Our Twin Peaks-y Kino seats 8 people and The Videodrome seats up to
             18. For more details or to check availability and book a screening
           </p>
+          <Image
+            src={videodromeCinema}
+            alt="Cinema auditorium in the style of bladerunner"
+            className="border border-primary mb-4"
+          />
           <StandardButton>Book the Videodrome</StandardButton>
         </div>
       </div>
