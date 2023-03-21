92ab49d84e99a82fdc6e087f7775f772e53c56ba
Alec Hamilton <96293544+alec-hamilton@users.noreply.github.com>
2023-03-21 11:40:06 +0000

    Feature/about/rent by post (#6)
    * refactor: page content wrapper margins
    
    * feat: rent by post done, refactoring to global constants
    
    * refactor: cinemas page using global constants

--- pages/about/the-shop.tsx
+++ pages/about/the-shop.tsx
@@ -1,66 +1,54 @@
 import Image from "next/image";
 import PageContentWrapper from "../components/surfaces/PageContentWrapper";
-import StandardButton from "../components/buttons/StandardButton";
+import PageContentHeader from "../components/surfaces/PageContentHeader";
 import shopFront from "../../assets/images/bladerunner-style-movie-store.png";
+import globalConstants from "@/constants/globalConstants";
+
+const {
+  theShop: {
+    pageTitle,
+    headerText,
+    buttonOneText,
+    buttonTwoText,
+    sectionOne,
+    sectionTwo,
+    sectionThree,
+  },
+} = globalConstants;
 
 const TheShop = () => {
   return (
     <PageContentWrapper>
-      <section className="p-4 md:p-6">
-        <h1>The Shop</h1>
-        <div className="flex flex-col sm:flex-row sm:gap-6">
-          <div className="flex flex-col sm:py-2">
-            <p className="py-4 sm:pb-8">
-              Our Twin Peaks-y Kino seats 8 people and The Videodrome seats up
-              to 18. For more details or to check availability and book a
-              screening.. COPY CHANGE NEEDED?
-            </p>
-            <div className="flex flex-col gap-3 mb-4 sm:mb-1 sm:flex-row">
-              <StandardButton>Browse our collection</StandardButton>
-              <StandardButton>Take virtual tour</StandardButton>
-            </div>
-          </div>
-          <Image
-            src={shopFront}
-            alt="Movie rental store shop front in the style of bladerunner"
-            className="border border-primary sm:w-4/12 sm:mt-4"
-          />
-        </div>
-      </section>
-      <div className="flex flex-col gap-4 mx-4 mb-4 sm:flex-row sm:gap-6 md:mx-6 md:mb-6">
+      <PageContentHeader
+        title={pageTitle}
+        text={headerText}
+        buttonOneText={buttonOneText}
+        buttonTwoText={buttonTwoText}
+      >
+        <Image
+          src={shopFront}
+          alt="Movie rental store shop front in the style of bladerunner"
+          className="border border-primary sm:w-4/12"
+        />
+      </PageContentHeader>
+      <div className="flex flex-col gap-4 my-4 md:my-6 sm:flex-row sm:gap-6">
         <section className="p-4 md:p-6 border border-primary bg-black2 sm:w-6/12">
-          <h2 className="pb-4">Address</h2>
+          <h2 className="pb-4">{sectionOne.title}</h2>
           <address className="not-italic">
-            <p>20th Century Flicks</p>
-            <p>19 Christmas Steps</p>
-            <p>Bristol</p>
-            <p>BS1 5BS</p>
+            <p>{sectionOne.content.line1}</p>
+            <p>{sectionOne.content.line2}</p>
+            <p>{sectionOne.content.line3}</p>
+            <p>{sectionOne.content.line4}</p>
           </address>
         </section>
         <section className="p-4 md:p-6 border border-primary bg-black2 sm:w-6/12">
-          <h2 className="pb-4">Opening times</h2>
-          <p>
-            Unfixed for the time being! Usually Dave is in 4ish til 7ish pm,
-            Monday to Thursday and PG is in Friday evenings and Saturday and
-            Sunday daytimes. Phone or email to check!
-          </p>
+          <h2 className="pb-4">{sectionTwo.title}</h2>
+          <p>{sectionTwo.content}</p>
         </section>
       </div>
-      <section className="p-4 md:p-6 border border-primary bg-black2 mx-4 mb-4 md:mx-6 md:mb-6">
-        <h2 className="pb-4">Accessibility</h2>
-        <p>
-          The shop is on Christmas Steps, a historic, cobbled pedestrian street
-          in the centre of Bristol. There are 22 steps up to the shop from
-          Colston Avenue and 25 steps down to the shop from Colston Street.
-          There is a loading bay at the bottom of the Christmas Steps on Colston
-          Avenue which can be used as a parking space to drop off or pick up
-          movies from the shop. The steps are uneven and there isn’t a handrail.
-          This is because the street is historical and listed and can’t be
-          changed! In the shop, we have chairs for customers. We have a urinal
-          and a toilet at the shop. There is one step to the urinal and 5 to the
-          toilet. The shop can be very busy on Saturdays, the weekdays are much
-          quieter.
-        </p>
+      <section className="p-4 md:p-6 border border-primary bg-black2">
+        <h2 className="pb-4">{sectionThree.title}</h2>
+        <p>{sectionThree.content}</p>
       </section>
     </PageContentWrapper>
   );
--- pages/cinemas.tsx
+++ pages/cinemas.tsx
@@ -4,42 +4,42 @@ import twinPeaksCinema from "../assets/images/twin-peaks-style-cinema.png";
 import videodromeCinema from "../assets/images/bladerunner-style-cinema.png";
 import shopFront from "../assets/images/bladerunner-style-movie-store.png";
 import PageContentWrapper from "./components/surfaces/PageContentWrapper";
+import PageContentHeader from "./components/surfaces/PageContentHeader";
+import globalConstants from "@/constants/globalConstants";
+
+const {
+  cinemas: {
+    pageTitle,
+    headerText,
+    buttonOneText,
+    buttonTwoText,
+    sectionOne,
+    sectionTwo,
+  },
+} = globalConstants;
 
 const Cinemas = () => {
   return (
     <PageContentWrapper>
-      <section className="p-4 md:p-6">
-        <h1>Our Cinemas</h1>
-        <div className="flex flex-col sm:flex-row sm:gap-6">
-          <div className="flex flex-col sm:py-2">
-            <p className="py-4 sm:pb-8">
-              Our Twin Peaks-y Kino seats 8 people and The Videodrome seats up
-              to 18. For more details or to check availability and book a
-              screening.. COPY CHANGE NEEDED?
-            </p>
-            <div className="flex flex-col gap-3 mb-4 sm:mb-1 sm:flex-row">
-              <StandardButton>Book a cinema</StandardButton>
-              <StandardButton>Take virtual tour</StandardButton>
-            </div>
-          </div>
-          <Image
-            src={shopFront}
-            alt="Movie rental store shop front in the style of bladerunner"
-            className="border border-primary sm:w-4/12 sm:mt-4"
-          />
-        </div>
-      </section>
-      <section className="p-4 m-4 md:m-6 md:p-6 border border-primary bg-black2">
-        <h2>The Kino</h2>
+      <PageContentHeader
+        title={pageTitle}
+        text={headerText}
+        buttonOneText={buttonOneText}
+        buttonTwoText={buttonTwoText}
+      >
+        <Image
+          src={shopFront}
+          alt="Movie rental store shop front in the style of bladerunner"
+          className="border border-primary sm:w-4/12"
+        />
+      </PageContentHeader>
+      <section className="p-4 md:p-6 my-4 md:my-6 border border-primary bg-black2">
+        <h2>{sectionOne.title}</h2>
         <div className="flex flex-col sm:flex-row sm:gap-6">
           <div>
-            <p className="py-4 ">
-              Our Twin Peaks-y Kino seats 8 people and The Videodrome seats up
-              to 18. For more details or to check availability and book a
-              screening
-            </p>
+            <p className="py-4 ">{sectionOne.content}</p>
             <div className="mb-4">
-              <StandardButton>Book the Kino</StandardButton>
+              <StandardButton>{sectionOne.button}</StandardButton>
             </div>
           </div>
           <Image
@@ -49,17 +49,13 @@ const Cinemas = () => {
           />
         </div>
       </section>
-      <section className="p-4 m-4 md:m-6 md:p-6 border border-primary bg-black2">
-        <h2>The Videodrome</h2>
+      <section className="p-4 md:p-6 border border-primary bg-black2">
+        <h2>{sectionTwo.title}</h2>
         <div className="flex flex-col sm:flex-row sm:gap-6">
           <div>
-            <p className="py-4">
-              Our Twin Peaks-y Kino seats 8 people and The Videodrome seats up
-              to 18. For more details or to check availability and book a
-              screening
-            </p>
+            <p className="py-4">{sectionTwo.content}</p>
             <div className="mb-4">
-              <StandardButton>Book the Videodrome</StandardButton>
+              <StandardButton>{sectionTwo.button}</StandardButton>
             </div>
           </div>
           <Image
