3f50391275d2c3bcb0d21f80e84756a27dfb6e01
alech <hamiltonalec@gmail.com>
2023-03-27 10:14:57 +0100

    feat: modal opening on desktop

--- components/Layout/Navbar/components/NavSearchButton.tsx
+++ components/Layout/Navbar/components/NavSearchButton.tsx
@@ -1,18 +1,27 @@
+import SearchModal from "@/components/modals/SearchModal";
+import { useState } from "react";
 import { BiSearch } from "react-icons/bi";
 
 const NavSearchButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <button
-      type="button"
-      className="border border-fuchsia-400 hover:bg-fuchsia-400/20 px-4 bg-black1"
-    >
-      <span className="flex gap-4">
-        <span>
-          <BiSearch size="1.5rem" className="text-fuchsia-400" />
+    <>
+      <button
+        onClick={() => setIsOpen(true)}
+        type="button"
+        className="border border-fuchsia-400 hover:bg-fuchsia-400/20 px-4 bg-black1"
+      >
+        <span className="flex gap-4">
+          <span>
+            <BiSearch size="1.5rem" className="text-fuchsia-400" />
+          </span>
+          <span className="font-bold text-fuchsia-400">Search</span>
         </span>
-        <span className="font-bold text-fuchsia-400">Search</span>
-      </span>
-    </button>
+      </button>
+      <SearchModal isOpen={isOpen} handleClose={() => setIsOpen(false)}>
+        This is the modal content!
+      </SearchModal>
+    </>
   );
 };
 
--- components/modals/SearchModal.tsx
+++ components/modals/SearchModal.tsx
@@ -1,5 +1,23 @@
-const SearchModal = () => {
-  return (  );
-}
- 
+import ReactPortal from "./ReactPortal";
+
+type SearchModalProps = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  handleClose: () => void;
+};
+
+const SearchModal = ({ children, isOpen, handleClose }: SearchModalProps) => {
+  if (!isOpen) return null;
+  return (
+    <ReactPortal wrapperId="react-portal-modal">
+      <div className="inset-0 fixed bg-black/80">
+        <button type="button" className="text-primary" onClick={handleClose}>
+          Close
+        </button>
+        <div>{children}</div>
+      </div>
+    </ReactPortal>
+  );
+};
+
 export default SearchModal;
