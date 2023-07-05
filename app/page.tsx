aea85cb63d8b842b27872aac1e27c246e4252350
alech <hamiltonalec@gmail.com>
2023-07-05 10:57:23 +0100

    feat: image responsive in staff faves

--- app/page.tsx
+++ app/page.tsx
@@ -33,16 +33,20 @@ export default function Page() {
       <section className="p-4 md:p-6 my-4 md:my-6 border border-primary bg-black2">
         <h2 className="pb-4 md:pb-6">Staff favorites</h2>
         <div className="flex flex-col gap-4 sm:flex-row">
-          <Image
-            src="https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-            alt="blade runner movie poster"
-            width={355}
-            height={320}
-            className="border border-primary"
-          />
+          <div className="relative w-full aspect-[100/149] self-center">
+            <Image
+              src="https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+              alt="blade runner movie poster"
+              fill
+              className="border border-primary"
+            />
+          </div>
+
           <div className="flex flex-col gap-2">
             <h3>Blade runner</h3>
             <p>
+              A blade runner must pursue and terminate four replicants who stole
+              a ship in space and have returned to Earth to find their creator.
               A blade runner must pursue and terminate four replicants who stole
               a ship in space and have returned to Earth to find their creator.
             </p>
