25cc8917c8b72a39cab2128e696154d7fa573909
alech <hamiltonalec@gmail.com>
2023-07-05 11:20:48 +0100

    feat: staff favorites added

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import ExternalLink from "@/components/links/ExternalLink";
 import StandardButton from "@/components/buttons/StandardButton";
 import PageContentHeader from "@/components/surfaces/PageContentHeader";
 import NewToRent from "@/components/tables/NewToRent";
+import StaffFavorites from "@/components/sections/StaffFavorites";
 import { globalConstants } from "@/constants/globalConstants";
-import Image from "next/image";
 
 const {
   homePage: { pageTitle, headerText },
@@ -30,29 +30,7 @@ export default function Page() {
         </ExternalLink>
       </PageContentHeader>
       <NewToRent />
-      <section className="p-4 md:p-6 my-4 md:my-6 border border-primary bg-black2">
-        <h2 className="pb-4 md:pb-6">Staff favorites</h2>
-        <div className="flex flex-col gap-4 sm:flex-row">
-          <div className="relative w-full aspect-[100/149] self-center">
-            <Image
-              src="https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-              alt="blade runner movie poster"
-              fill
-              className="border border-primary"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h3>Blade runner</h3>
-            <p>
-              A blade runner must pursue and terminate four replicants who stole
-              a ship in space and have returned to Earth to find their creator.
-              A blade runner must pursue and terminate four replicants who stole
-              a ship in space and have returned to Earth to find their creator.
-            </p>
-          </div>
-        </div>
-      </section>
+      <StaffFavorites />
     </PageContentWrapper>
   );
 }
