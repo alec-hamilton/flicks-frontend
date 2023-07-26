e81163ba4c25ba37ed383c7d42d7628d1b13f20d
alech <hamiltonalec@gmail.com>
2023-07-26 14:18:17 +0100

    feat: responsive page

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -51,42 +51,40 @@ const movieData = {
 const MoviePage = async ({ params: { id } }: MoviePageProps) => {
   // const movieData = await fetchMovie(id);
 
-  const separateGenres = (genres: string) => {
-    return genres.split(",");
-  };
-
   return (
     <PageContentWrapper>
-      <div className="grid grid-cols-6 gap-x-8">
-        <div className="flex flex-col col-span-2 border border-primary">
+      <div className="flex flex-col-reverse md:grid md:grid-cols-6 gap-x-8">
+        <div className="flex flex-col xs:grid xs:grid-cols-2 md:flex-col md:flex md:col-span-2 border border-primary">
           <Image
             src={movieData.Poster}
-            width="300"
+            width="400"
             height="400"
             alt={`movie poster for ${movieData.Title}`}
           />
-          <div className="flex justify-between">
-            <h4>Cert</h4>
-            <p>{movieData.Rated}</p>
-          </div>
-          <div className="flex justify-between">
-            <h4>Running time</h4>
-            <p>{movieData.Runtime}</p>
-          </div>
-          <div className="flex justify-between">
-            <h4>Country</h4>
-            <p>{movieData.Country}</p>
-          </div>
-          <div className="flex justify-between">
-            <h4>Language</h4>
-            <p>{movieData.Language}</p>
+          <div className="flex flex-col gap-y-4 p-4 xs:border-l md:border-t border-primary">
+            <div className="flex justify-between gap-x-2">
+              <h4>Cert</h4>
+              <p className="text-sm text-end">{movieData.Rated}</p>
+            </div>
+            <div className="flex justify-between gap-x-2">
+              <h4>Running time</h4>
+              <p className="text-sm text-end">{movieData.Runtime}</p>
+            </div>
+            <div className="flex justify-between gap-x-2">
+              <h4>Country</h4>
+              <p className="text-sm text-end">{movieData.Country}</p>
+            </div>
+            <div className="flex justify-between gap-x-2">
+              <h4>Language</h4>
+              <p className="text-sm text-end">{movieData.Language}</p>
+            </div>
           </div>
         </div>
         <div className="flex flex-col col-span-4">
-          <h1>{`${movieData.Title} (${movieData.Year})`}</h1>
+          <h1 className="pb-3">{`${movieData.Title} (${movieData.Year})`}</h1>
           <h2>{`Directed by ${movieData.Director}`}</h2>
-          <div className="flex">
-            {separateGenres(movieData.Genre).map((genre) => {
+          <div className="flex gap-x-2 my-4">
+            {movieData.Genre.split(",").map((genre) => {
               return (
                 <div className="border border-primary p-2" key={genre}>
                   {genre}
@@ -94,10 +92,14 @@ const MoviePage = async ({ params: { id } }: MoviePageProps) => {
               );
             })}
           </div>
-          <h3>Cast</h3>
-          <p>{movieData.Actors}</p>
-          <h3>Plot</h3>
-          <p>{movieData.Plot}</p>
+          <div className="mb-4">
+            <h3>Cast</h3>
+            <p>{movieData.Actors}</p>
+          </div>
+          <div className="mb-4">
+            <h3>Plot</h3>
+            <p>{movieData.Plot}</p>
+          </div>
         </div>
       </div>
     </PageContentWrapper>
