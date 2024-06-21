ee156664dd158cd3c9101289a9ce15cc4e282789
alec-hamilton <hamiltonalec@gmail.com>
2024-06-21 13:27:58 +0100

    feat: make ts happy

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import PageContentWrapper from "@/components/surfaces/PageContentWrapper";
 import { createClient } from "@/lib/supabase/server";
+import { getDirectors, getNonDirectors } from "@/lib/helpers/moviePage";
 
 type MoviePageProps = {
   params: {
@@ -8,7 +9,7 @@ type MoviePageProps = {
   };
 };
 
-const API_KEY: string = process.env.OMDB_API_KEY as string;
+// const API_KEY: string = process.env.OMDB_API_KEY as string;
 
 // const fetchMovie = async (id: string) => {
 //   const DATA_SOURCE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`;
@@ -23,23 +24,22 @@ const API_KEY: string = process.env.OMDB_API_KEY as string;
 // };
 
 const fetchMovie = async (id: string) => {
-
   const supabase = createClient();
-  const {
-    data: [movieData],
-    error,
-  } = await supabase
+  const { data: movieData, error } = await supabase
     .from("titles")
     .select(
       "*, categories:titles2categories(categories(id, description)), languages:titles2languages(languages(id, language)), nationalities:titles2nationalities(nationalities(id, country)), people:titles2people(people(id, forename, surname), roles(id, role_name))"
     )
-    .eq("id", id);
+    .eq("id", id)
+    .single();
 
-    return { movieData, error };
-}
+  return { movieData, error };
+};
 
 export const generateMetadata = async ({ params }: MoviePageProps) => {
-  const {movieData} = await fetchMovie(params.id);
+  const { movieData } = await fetchMovie(params.id);
+
+  if (!movieData) return;
 
   // TODO: uncapitalise titles.
   return {
@@ -49,101 +49,92 @@ export const generateMetadata = async ({ params }: MoviePageProps) => {
 };
 
 const MoviePage = async ({ params: { id } }: MoviePageProps) => {
- const { movieData, error } = await fetchMovie(id);
+  const { movieData, error } = await fetchMovie(id);
 
-  const getDirectors = (entries: Entry[]): PersonWithId[] =>
-    entries
-      .filter((entry) => entry.roles.id === 1)
-      .map((entry) => ({
-        id: entry.people.id,
-        name: `${entry.people.forename.trim()} ${entry.people.surname.trim()}`,
-      }));
+  if (error) return <p>{error.message}</p>;
 
-  const getNonDirectors = (entries: Entry[]): PersonWithId[] =>
-    entries
-      .filter((entry) => entry.roles.id > 1)
-      .map((entry) => ({
-        id: entry.people.id,
-        name: `${entry.people.forename.trim()} ${entry.people.surname.trim()}`,
-      }));
-
-  return (
-    <PageContentWrapper>
-      <div className="flex flex-col-reverse md:grid md:grid-cols-6 gap-x-8">
-        <div className="flex flex-col xs:grid xs:grid-cols-2 md:flex-col md:flex md:col-span-2 border border-foreground">
-          {/* <Image
+  if (movieData)
+    return (
+      <PageContentWrapper>
+        <div className="flex flex-col-reverse md:grid md:grid-cols-6 gap-x-8">
+          <div className="flex flex-col xs:grid xs:grid-cols-2 md:flex-col md:flex md:col-span-2 border border-foreground">
+            {/* <Image
             src={movieData.Poster}
             width="290"
             height="430"
             alt={`movie poster for ${movieData.Title}`}
             priority
           /> */}
-          <div className="flex flex-col gap-y-4 p-4 xs:max-md:border-l md:border-t border-foreground">
-            <div className="flex justify-between gap-x-2">
-              <h4>Cert</h4>
-              <p className="text-sm text-end">
-                {movieData.certification}
-              </p>
-            </div>
-            <div className="flex justify-between gap-x-2">
-              <h4>Running time</h4>
-              <p className="text-sm text-end">
-                {movieData.runningtime}
-              </p>
-            </div>
-            <div className="flex justify-between gap-x-2">
-              <h4>Country</h4>
-              <p className="text-sm text-end">
-                {movieData.nationalities.map((country) => (
-                  <span>{country.nationalities.country}</span>
-                ))}
-              </p>
-            </div>
-            <div className="flex justify-between gap-x-2">
-              <h4>Language</h4>
-              <p className="text-sm text-end">
-                {movieData.languages.map((language) => (
-                  <span>{language.languages.language}</span>
-                ))}
-              </p>
+            <div className="flex flex-col gap-y-4 p-4 xs:max-md:border-l md:border-t border-foreground">
+              <div className="flex justify-between gap-x-2">
+                <h4>Cert</h4>
+                <p className="text-sm text-end">{movieData.certification}</p>
+              </div>
+              <div className="flex justify-between gap-x-2">
+                <h4>Running time</h4>
+                <p className="text-sm text-end">{movieData.runningtime}</p>
+              </div>
+              <div className="flex justify-between gap-x-2">
+                <h4>Country</h4>
+                <p className="text-sm text-end">
+                  {movieData.nationalities.map(
+                    ({ nationalities }) =>
+                      nationalities && (
+                        <span key={nationalities.id}>
+                          {nationalities.country}
+                        </span>
+                      )
+                  )}
+                </p>
+              </div>
+              <div className="flex justify-between gap-x-2">
+                <h4>Language</h4>
+                <p className="text-sm text-end">
+                  {movieData.languages.map(
+                    ({ languages }) =>
+                      languages && (
+                        <span key={languages.id}>{languages.language}</span>
+                      )
+                  )}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col col-span-4">
-          <h1 className="pb-3">{`${movieData.title} (${movieData.date_1})`}</h1>
-          <h2>
-            {getDirectors(movieData.people).map((director) => (
-              <span>{director.name}</span>
-            ))}
-          </h2>
-          <div className="flex gap-x-2 my-4">
-            {movieData.categories.map((category) => {
-              return (
-                <div
-                  className="border border-foreground p-2"
-                  key={category.categories.id}
-                >
-                  {category.categories.description}
-                </div>
-              );
-            })}
-          </div>
-          <div className="mb-4">
-            <h3>Cast</h3>
-            <p>
-              {getNonDirectors(movieData.people).map((castMember) => (
-                <span>{castMember.name}</span>
+          <div className="flex flex-col col-span-4">
+            <h1 className="pb-3">{`${movieData.title} (${movieData.date_1})`}</h1>
+            <h2>
+              {getDirectors(movieData.people).map(({ name, id }) => (
+                <span key={id}>{name}</span>
               ))}
-            </p>
-          </div>
-          <div className="mb-4">
-            <h3>Plot</h3>
-            <p>{movieData.review}</p>
+            </h2>
+            <div className="flex gap-x-2 my-4">
+              {movieData.categories.map(({ categories }) => {
+                return (
+                  <div
+                    className="border border-foreground p-2"
+                    key={categories?.id}
+                  >
+                    {categories?.description}
+                  </div>
+                );
+              })}
+            </div>
+            <div className="mb-4">
+              <h3>Cast</h3>
+              <p>
+                {getNonDirectors(movieData.people).map(({ name, id }) => (
+                  <span key={id}>{name}</span>
+                ))}
+              </p>
+            </div>
+            <div className="mb-4">
+              <h3>Plot</h3>
+              <p>{movieData.review}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </PageContentWrapper>
-  );
+      </PageContentWrapper>
+    );
 };
 
 export default MoviePage;
